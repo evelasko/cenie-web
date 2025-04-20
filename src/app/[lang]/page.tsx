850d99d142d7bb775147964ad08cccd8adb0e69d
Enrique Velasco <h.superpotter@gmail.com>
2025-04-21 00:38:42 +0200

    fixed language change in home page

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,21 +1,25 @@
-import { type Metadata } from 'next';
+import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 import { asImageSrc } from '@prismicio/client';
 import { SliceZone } from '@prismicio/react';
 
 import { createClient } from '@/prismicio';
 import { components } from '@/slices';
-import Layout from '@/components/Page';
+import Layout from '@/components/Layout';
+import { locales } from '@/constants/locales';
 
 type Params = { lang: string };
 
 export default async function Page({ params }: { params: Promise<Params> }) {
-  const client = createClient();
   const { lang } = await params;
+
+  const client = createClient();
   const page = await client.getSingle('home_page', { lang }).catch(() => notFound());
 
+  const localizedUrls = Object.keys(locales).reduce((acc, l) => ({ ...acc, [l]: `/${l}` }), {});
+
   return (
-    <Layout>
+    <Layout lang={lang} localizedUrls={localizedUrls}>
       <SliceZone slices={page.data.slices} components={components} />
     </Layout>
   );
